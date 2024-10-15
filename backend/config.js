4c730d39560cde4085d5b675647c2e11427a0ec3
Crupercio <Luis.f.cruzsanchez@outlook.com>
2024-10-14 20:54:47 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 export const mongoDB_URL = 
-    'mongodb+srv://admin_book:2K2o1SOZBCQjV6lO@pokecombos.dnpxi.mongodb.net/?retryWrites=true&w=majority&appName=PokeCombos'
+    'mongodb+srv://user:password@cluster.dnpxi.mongodb.net/?retryWrites=true&w=majority&appName'
